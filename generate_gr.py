3a0f31309418241275fa4f356f92e3bce9022c15
Mati02133 <mateuszjasek28@gmail.com>
2026-07-26 20:01:43 +0200

    fix: change url link

--- generate_gr.py
+++ generate_gr.py
@@ -2,13 +2,15 @@ import qrcode
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 access_code = os.environ.get("ACCESS_CODE")
-base_url = "http://127.0.0.1:5000" 
+base_url = "https://mati02133.eu.pythonanywhere.com"
+
 link = f"{base_url}/access?code={access_code}"
 
 img = qrcode.make(link)
-img.save("dostep_qr.png")
+img.save(os.path.join(BASE_DIR, "dostep_qr.png"))
 
 print(f"QR code generated for link: {link}")
