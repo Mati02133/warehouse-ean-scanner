0c2ae2261cf912fd3059ec030b27f3614ef1a2fa
Mati02133 <mateuszjasek28@gmail.com>
2026-07-26 14:34:13 +0200

    feat: add excel import script with EAN as string type

--- import_excel.py
+++ import_excel.py
@@ -1,2 +1,21 @@
-import pandas as pd 
+import pandas 
 import sqlite3
+
+df = pandas.read_excel('magazyn.ods', engine='odf', dtype={"ind_kod_kreskowy": str}) # We need to specify ean beacuse if there are 0s at the beginning of the ean, they will be removed
+
+df["ind_kod_kreskowy"] = df["ind_kod_kreskowy"].str.replace(".0", "", regex=False) # Remove the ".0" from the ean values
+
+df = df.rename(columns={"ind_nazwa": "name", "ind_kod_kreskowy": "ean", "wms_pozycja": "location"})
+
+df["ean"] = df["ean"].fillna("") # Fill NaN (Not a number) values in the ean column with an empty string
+
+df["location"] = df["location"].fillna("") # Same thing
+ 
+conn = sqlite3.connect('database.db') # Create a connection to the SQLite database
+
+df.to_sql('products', conn, if_exists='replace', index=False) # Convert the DataFrame to a SQL table
+
+conn.close() 
+
+print(f"Data imported successfully into the database. Total records: {len(df)}")
+
